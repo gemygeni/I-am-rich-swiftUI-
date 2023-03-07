6ed8ce8336991a5532787db337b2d753d359e5fd
Ahmed Gamal <ahmedgamal.miner@gmail.com>
2023-03-08 01:21:02 +0200

    Initial Commit

---
+++ I am rich(swiftUI)/I_am_rich_swiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_rich_swiftUI_App.swift
+//  I am rich(swiftUI)
+//
+//  Created by AHMED GAMAL  on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct I_am_rich_swiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
